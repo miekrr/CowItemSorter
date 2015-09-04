4a6888d8edfb8abd6af1cd97cfe53ee24d71ca15
miekrr <suprarealistul@gmail.com>
2015-09-05 02:05:13 +0300

    Actual implementation of Cow Items used to filter the data set.

--- src/main/java/CowClothes.java
+++ src/main/java/CowClothes.java
@@ -2,4 +2,19 @@
  * Created by mike on 8/25/2015.
  */
 public class CowClothes extends CowItem {
+
+    double goldRate;
+    double goldPercent;
+
+    @Override
+    public double getItemValue() {
+
+        if(goldPercent == 0)
+            return goldRate;
+
+        if(goldRate == 0 )
+            return goldPercent;
+
+        return goldRate * goldPercent;
+    }
 }
