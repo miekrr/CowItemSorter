2c0afda00fa9eb26bebfa3448a267da4cb079c00
miekrr <suprarealistul@gmail.com>
2015-09-07 23:12:48 +0300

    Moved key attribute parser to a new method.

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -14,7 +14,7 @@ public class Parser {
     private String hData;
     List<CowItem> cowItems = new LinkedList<>();
 
-    Parser(String filePath){
+    public Parser(String filePath){
         byte[] data = new byte[0];
         try {
             data = Files.readAllBytes(Paths.get(filePath));
@@ -24,7 +24,78 @@ public class Parser {
         hData = new String(data);
     }
 
-    public void getItems() {
+    private boolean setDataForCowItem(Element e, CowClothes item, Attribute a) {
+        switch(a.getKey()) {
+            case "data-item-id":
+                item.setItemName(a.getValue());
+                break;
+            case "data-original-title":
+                Element element = e.html(a.getValue());
+                Element dat = element.children().get(3);
+                List<TextNode> str = dat.textNodes();
+
+                if (item.getItemName().startsWith("death") && !item.getItemName().endsWith("dust"))
+                {
+                    String strGoldPerDeath = element.getAllElements().get(6).childNodes().get(1).toString()
+                            .trim();
+                    strGoldPerDeath = strGoldPerDeath.substring(0, strGoldPerDeath.indexOf(" "));
+                    item.goldRate =  Double.parseDouble(strGoldPerDeath);
+                    return true;
+                }
+
+                if (item.getItemName().startsWith("cow"))
+                {
+                    if(str.size() == 3) {
+                        String strGoldHour = str.get(1).toString().trim();
+                        String strGoldRate = str.get(2).toString().trim();
+
+                        String s = strGoldHour.substring(0, strGoldHour.indexOf(" "));
+                        double goldPerHour = Double.parseDouble(s);
+                        item.goldPercent = goldPerHour;
+                        s = strGoldRate.substring(0, strGoldRate.indexOf("%"));
+
+                        double goldGain = Double.parseDouble(s);
+                        item.goldRate =  goldGain;
+                        return true;
+                    }
+                }
+                if (item.getItemName().startsWith("pax_")) {
+                    element.getAllElements();
+                    String strGoldHour = element.getAllElements().get(6).childNodes().get(1).toString().trim();
+                    String strGoldRate = element.getAllElements().get(6).childNodes().get(4).toString().trim();
+
+                    String s = strGoldHour.substring(0, strGoldHour.indexOf(" "));
+                    double goldPerHour = Double.parseDouble(s);
+                    item.goldPercent = goldPerHour;
+                    s = strGoldRate.substring(0, strGoldRate.indexOf("%"));
+
+                    double goldGain = Double.parseDouble(s);
+                    item.goldRate =  goldGain;
+                    return true;
+                }
+                if (item.getItemName().startsWith("bull")  && !item.getItemName().endsWith("box") &&
+                        item.getItemName().compareTo("bullgrillz") != 0) {
+                    String strGoldRate = element.getAllElements().get(5).childNodes().get(4).toString().trim();
+                    String s = strGoldRate.substring(0, strGoldRate.indexOf(" "));
+                    double goldPerHour = Double.parseDouble(s);
+                    item.goldPercent = goldPerHour;
+                    return true;
+                }
+                break;
+            case "data-item-uid":
+                item.setItemUID(a.getValue());
+                break;
+            case "data-item-id-numeric":
+                item.setItemID(a.getValue());
+                break;
+            case "data-item-recycle":
+                item.setItemRecyclable(a.getValue().compareTo("1") == 0 ? true : false);
+                break;
+        }
+        return false;
+    }
+
+    private void parseHtml() {
         Document doc = Jsoup.parse(hData);
 
         Elements items = doc.getElementsByClass("inventory-slot");
@@ -36,76 +107,6 @@ public class Parser {
             boolean validItem = false;
             for(Attribute a : attribs)
             {
-                switch(a.getKey()) {
-                    case "data-item-id":
-                        System.out.println( a.getValue() );
-                        item.setItemName(a.getValue());
-                        break;
-                    case "data-original-title":
-                        Element element = e.html(a.getValue());
-                        Element dat = element.children().get(3);
-                        List<TextNode> str = dat.textNodes();
-
-                        if (item.getItemName().startsWith("death") && !item.getItemName().endsWith("dust"))
-                        {
-                            String strGoldPerDeath = element.getAllElements().get(6).childNodes().get(1).toString()
-                                    .trim();
-                            strGoldPerDeath = strGoldPerDeath.substring(0, strGoldPerDeath.indexOf(" "));
-                            item.goldRate =  Double.parseDouble(strGoldPerDeath);
-                            validItem = true;
-                        }
-
-                        if (item.getItemName().startsWith("cow"))
-                        {
-                            if(str.size() == 3) {
-                                String strGoldHour = str.get(1).toString().trim();
-                                String strGoldRate = str.get(2).toString().trim();
-
-                                String s = strGoldHour.substring(0, strGoldHour.indexOf(" "));
-                                double goldPerHour = Double.parseDouble(s);
-                                item.goldPercent = goldPerHour;
-                                s = strGoldRate.substring(0, strGoldRate.indexOf("%"));
-
-                                double goldGain = Double.parseDouble(s);
-                                item.goldRate =  goldGain;
-                                System.out.println("Gold per hour: " + goldPerHour + " gain: " + goldGain);
-                                validItem = true;
-                            }
-                        }
-                        if (item.getItemName().startsWith("pax_")) {
-                            element.getAllElements();
-                            String strGoldHour = element.getAllElements().get(6).childNodes().get(1).toString().trim();
-                            String strGoldRate = element.getAllElements().get(6).childNodes().get(4).toString().trim();
-
-                            String s = strGoldHour.substring(0, strGoldHour.indexOf(" "));
-                            double goldPerHour = Double.parseDouble(s);
-                            item.goldPercent = goldPerHour;
-                            s = strGoldRate.substring(0, strGoldRate.indexOf("%"));
-
-                            double goldGain = Double.parseDouble(s);
-                            item.goldRate =  goldGain;
-                            System.out.println("Gold per hour: " + goldPerHour + " gain: " + goldGain);
-                            validItem = true;
-                        }
-                        if (item.getItemName().startsWith("bull")  && !item.getItemName().endsWith("box") &&
-                                item.getItemName().compareTo("bullgrillz") != 0) {
-                            String strGoldRate = element.getAllElements().get(5).childNodes().get(4).toString().trim();
-                            String s = strGoldRate.substring(0, strGoldRate.indexOf(" "));
-                            double goldPerHour = Double.parseDouble(s);
-                            item.goldPercent = goldPerHour;
-                            validItem = true;
-                        }
-                        break;
-                    case "data-item-uid":
-                        item.setItemUID(a.getValue());
-                        break;
-                    case "data-item-id-numeric":
-                        item.setItemID(a.getValue());
-                        break;
-                    case "data-item-recycle":
-                        item.setItemRecyclable(a.getValue().compareTo("1") == 0 ? true : false);
-                        break;
-                }
             }
             if(validItem)
                 cowItems.add(item);
