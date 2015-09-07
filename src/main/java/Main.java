da6b6021d8eea6e26892ebf11c8103a0b846dd88
miekrr <suprarealistul@gmail.com>
2015-09-07 23:11:24 +0300

    Make main accept input and out parameters.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,21 @@
 public class Main {
 
     public static void main(String[] args) {
-        Parser parser = new Parser("D:/SourceCode/cow-site-parser/inventory.html");
-        parser.getItems();
+
+        if( args.length == 2) {
+            Parser parser = new Parser("D:/SourceCode/cow-site-parser/inventory.html");
+            List<CowItem> cowItems = parser.getItems();
+
+        }
+        else
+        {
+            System.out.println("Usage: " +
+                    new java.io.File(Main.class.getProtectionDomain()
+                    .getCodeSource()
+                    .getLocation()
+                    .getPath())
+                    .getName() +
+                    " inventory.html items-to-be-deleted.txt");
+        }
     }
 }
